5a8a97761500dd6c5c283a597e8fb023f2448cf1
Bernard Arbogast <ben.arbogast@gmail.com>
2012-03-18 20:59:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRailsAptana::Application.config.secret_token = 'b52d2ec99d7fdd0ef302622c272accd366c2fc92f67e77f4527aef6dab66b503c07d7b5b212a2ccb86bbc06f49ac57042d90da245446c7962bac8a5db73dd4ea'
